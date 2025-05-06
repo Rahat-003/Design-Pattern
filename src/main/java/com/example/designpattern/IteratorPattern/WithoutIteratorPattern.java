41bda1cefc5a9994c02cfbf32b4328842532e8fe
rahat <rahatakhter4@gmail.com>
2025-05-06 11:48:15 +0600

    Without Iterator Pattern

--- src/main/java/com/example/designpattern/IteratorPattern/WithoutIteratorPattern.java
+++
@@ -1,7 +0,0 @@
-package com.example.designpattern.IteratorPattern;
-
-public class WithoutIteratorPattern {
-    public static void main(String[] args) {
-        System.out.println("-------- Without Iterator Pattern -------");
-    }
-}
