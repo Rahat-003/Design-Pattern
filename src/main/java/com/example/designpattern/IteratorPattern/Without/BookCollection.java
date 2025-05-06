a67df1029cb90761b2107ac730eb39a4f37c93e2
rahat <rahatakhter4@gmail.com>
2025-05-06 11:59:56 +0600

    Without Iterator pattern

--- src/main/java/com/example/designpattern/IteratorPattern/Without/BookCollection.java
+++ src/main/java/com/example/designpattern/IteratorPattern/Without/BookCollection.java
@@ -2,15 +2,13 @@ package com.example.designpattern.IteratorPattern.Without;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class BookCollection {
-    private Set<Book> books = new TreeSet<>();
+    private List<Book> books = new ArrayList<>();
     public void addBook(Book book){
         books.add(book);
     }
-    public Set<Book> getBooks(){
+    public List<Book> getBooks(){
         return books;
     }
 
